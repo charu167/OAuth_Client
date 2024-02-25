07f747487a33e35fee08f5efc9454515eba14df2
Charudatta Potdar <charupot08@gmail.com>
2024-02-25 11:53:57 -0700

    required scoepes added in google redirect URI

--- src/pages/GoogleAuth.jsx
+++ src/pages/GoogleAuth.jsx
@@ -7,7 +7,8 @@ export default function GoogleAuth() {
       "699400475494-hd4r32rnd7db5qmk2kr1nh30qmh93c3q.apps.googleusercontent.com";
     const redirectUri =
       "https://o-auth-callback-sla.vercel.app/api/callbackGoogle"; // Ensure this matches the one set in Google Cloud Console
-    const scope = "email profile openid";
+    const scope =
+      "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.channel-memberships.creator https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
     const responseType = "code";
 
     const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
