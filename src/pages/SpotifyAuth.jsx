3af56310fb5a8527186a0627db5e94ae35c42247
Charudatta Potdar <charupot08@gmail.com>
2024-02-27 15:11:29 -0700

    added appropriate scopes to spotify URL

--- src/pages/SpotifyAuth.jsx
+++ src/pages/SpotifyAuth.jsx
@@ -4,7 +4,7 @@ export default function SpotifyAuth() {
   const handleLogin = () => {
     const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
     const redirectUri = "https://o-auth-callback-sla.vercel.app/api/callbackSpotify";
-    const scope = "user-read-private user-read-email";
+    const scope = "user-read-private user-read-email playlist-modify-public playlist-modify-private playlist-read-collaborative";
     const responseType = "code";
 
     const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
