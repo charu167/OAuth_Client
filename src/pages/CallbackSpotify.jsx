0feb4a6b3348f709445f59d6173b5350be9fb5cd
Charudatta Potdar <charupot08@gmail.com>
2024-02-27 14:59:39 -0700

    spotify user ID fetching function implemented at login

--- src/pages/CallbackSpotify.jsx
+++ src/pages/CallbackSpotify.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -8,6 +9,20 @@ export default function CallbackSpotify() {
     (async () => {
       const access_token = await window.location.hash.split("=")[1];
       sessionStorage.setItem("spotify_access_token", access_token);
+
+      await axios
+        .get("https://api.spotify.com/v1/me", {
+          headers: {
+            Authorization: "Bearer " + access_token,
+          },
+        })
+        .then((res) => {
+          sessionStorage.setItem("spotifyID", res.data.id);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
       navigate("/dashboard");
     })();
   }, []);
